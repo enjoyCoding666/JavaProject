2b3c7cc729924d3f66a3bc204bb93893e5816a2b
fenglin <huaiyinnianshao28@163.com>
2017-12-22 17:24:30 +0800

    枚举的简单用法

---
+++ src/main/java/com/enumDemo/Color.java
@@ -0,0 +1,8 @@
+package com.enumDemo;
+
+/**
+ * Created by lenovo on  十二月
+ */
+public enum Color {
+    RED , WHITE , BLUE , BLACK
+}
---
+++ src/main/java/com/enumDemo/enumDemo.java
@@ -0,0 +1,16 @@
+package com.enumDemo;
+
+/**
+ * Created by lenovo on  十二月
+ */
+public class enumDemo {
+    public static void main(String[] args) {
+        System.out.println(Color.WHITE );
+        System.out.println(Color.BLACK);
+
+        for(ColorNumber colorNumber : ColorNumber.values()) {
+            System.out.println(colorNumber.getColor());
+            System.out.println(colorNumber.getNumber());
+        }
+    }
+}

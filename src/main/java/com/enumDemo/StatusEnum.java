c6dc933485d4310ef0bef0a902d926b3b3bbf27b
linfeng <475534279@qq.com>
2018-01-02 21:45:34 +0800

    同步锁的使用

--- src/main/java/com/enumDemo/StatusEnum.java
+++ src/main/java/com/enumDemo/StatusEnum.java
@@ -1,12 +1,12 @@
 package com.enumDemo;
 
-public enum Status {
+public enum StatusEnum {
     Success("报竣成功",9),Failure("报竣失败",-1),Wait("等待报竣",23),ProvinceFailure("省应答失败",22);
 
     private String status;
     private int number;
 
-    Status(String status, int number) {
+    StatusEnum(String status, int number) {
         this.status = status;
         this.number = number;
     }

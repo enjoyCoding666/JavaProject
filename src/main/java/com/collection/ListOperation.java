62ca2cf9b1d860dbd827bf2c56e77a0990708397
fenglin <huaiyinnianshao28@163.com>
2018-01-18 17:25:44 +0800

    list遍历

---
+++ src/main/java/com/collection/ListOperation.java
@@ -0,0 +1,17 @@
+package com.collection;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on  一月
+ */
+public class ListOperation {
+    /**
+     * 遍历List
+     */
+    public  void Ergodic(List<String> list){
+          for(String str : list) {
+              System.out.println(str);
+          }
+    }
+}

067211a9adf01902d268e6e27fd724eccf7a4e12
fenglin <475534279@qq.com>
2018-06-28 22:59:44 +0800

    Thread.yield()让所有线程继续竞争。

--- src/main/java/com/thread/YieldTest.java
+++ src/main/java/com/thread/YieldTest.java
@@ -1,13 +1,24 @@
 package com.thread;
 
-public class YieldTest {
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread1=new Thread( new MyThread("thread1") );
-        Thread thread2=new Thread( new MyThread("thread2") );
+public class YieldTest implements  Runnable{
+
+    @Override
+    public void run() {
+        for(int i=0;i<=10;i++) {
+            System.out.println(Thread.currentThread().getName()+",当前计数："+i);
+            if(5==i) {
+                Thread.yield();
+                System.out.println(Thread.currentThread().getName()+"执行yield，让所有线程继续竞争。");
+            }
+        }
+    }
+
+    public static void main(String[] args)  {
+        Thread thread1=new Thread( new YieldTest(),"thread1");
+        Thread thread2=new Thread( new YieldTest(),"thread2" );
         thread1.start();
         thread2.start();
 
-        //准备添加yeild()方法
-
     }
+
 }

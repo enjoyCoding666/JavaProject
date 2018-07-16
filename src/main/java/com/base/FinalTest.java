197687e1d7399bf75b2a1c86aa93754828ca58df
fenglin <huaiyinnianshao28@163.com>
2018-07-16 17:29:30 +0800

    final关键字的简单理解

---
+++ src/main/java/com/base/FinalTest.java
@@ -0,0 +1,14 @@
+package com.base;
+
+/**
+ * Created by lenovo on  七月
+ */
+public class FinalTest {
+    private static final  int  AGE=18;
+    private static final  String NAME="lin";
+
+    public static void main(String[] args) {
+//        AGE=28;                  //此处会报错，被修饰为final的基本类型数据的值，不可以改变。
+//          NAME="test";
+    }
+}

c81a4fe066c81470add8cf4816948b161a1f68a2
fenglin <huaiyinnianshao28@163.com>
2022-06-12 20:28:02 +0800

    测试

---
+++ src/test/java/com/string/CharAtTest.java
@@ -0,0 +1,19 @@
+package com.string;
+
+public class CharAtTest {
+
+    public static void main(String[] args) {
+//        String str = "adxfwf";
+//        char c = str.charAt(4);
+//        System.out.println(c);
+
+//        char[] chars = {'a','b','d'};
+//        String str = new String(chars);
+//        System.out.println(str);
+//
+
+        String str = "adxcxvw";
+        char[] chars = str.toCharArray();
+
+    }
+}

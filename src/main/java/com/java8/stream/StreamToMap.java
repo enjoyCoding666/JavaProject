53eb531923c7cad14212a128cd124b7a6dfff833
fenglin <huaiyinnianshao28@163.com>
2023-03-10 23:54:38 +0800

    Collectors.toMap处理重复key

--- src/main/java/com/java8/stream/StreamToMap.java
+++ src/main/java/com/java8/stream/StreamToMap.java
@@ -16,21 +16,47 @@ public class StreamToMap {
 
     public static void main(String[] args) {
         toMapTest();
+//        toMapValueBeanTest();
     }
+
 
     public static void toMapTest() {
         List<Worker> list = new ArrayList<>();
         list.add(new Worker("123",18,"lin"));
-        list.add(new Worker("456",28,"chen"));
-        list.add(new Worker("789",38,"wu"));
-        //key不可重复，否则会报错"Duplicate key"
-//        list.add(new Worker("123",18,"li"));
-        //value不可为null，否则会报错"空指针异常"
-//        list.add(new Worker("000",18,null));
+        list.add(new Worker("123",18,"li"));
+        list.add(new Worker("000",18,null));
+        //如果没有对重复key的处理 (k1,k2)->k2 ，那么key重复时会报错"Duplicate key"， 而且value为null时会报错"空指针异常"。
         Map<String, String> map = list.stream().collect(Collectors.toMap(Worker::getId, Worker::getName));
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey()+","+entry.getValue());
         }
     }
 
+    public static void toMapTest2() {
+        List<Worker> list = new ArrayList<>();
+        list.add(new Worker("123",18,"lin"));
+        list.add(new Worker("456",28,"chen"));
+        list.add(new Worker("789",38,"wu"));
+        list.add(new Worker("123",18,"li"));
+        list.add(new Worker("000",18,null));
+        //(k1,k2)->k2 处理重复key。如果k1和k2相同，就取k2对应的value
+        Map<String, String> map = list.stream().collect(Collectors.toMap(Worker::getId, Worker::getName, (k1,k2)->k2));
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey()+","+entry.getValue());
+        }
+    }
+
+
+    public static void toMapValueBeanTest() {
+        List<Worker> list = new ArrayList<>();
+        list.add(new Worker("123",18,"lin"));
+        list.add(new Worker("456",28,"chen"));
+        list.add(new Worker("123",18,"li"));
+        list.add(new Worker("000",18,null));
+        //value 为对象的map
+        Map<String, Worker> map = list.stream().collect(Collectors.toMap(Worker::getId, worker -> worker, (k1,k2)->k2));
+        for (Map.Entry<String, Worker> entry : map.entrySet()) {
+            System.out.println(entry.getKey()+","+entry.getValue());
+        }
+    }
 }

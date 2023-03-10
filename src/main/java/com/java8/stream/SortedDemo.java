0ef0df38b58fd5c2546230df945251e3eb14492f
fenglin <huaiyinnianshao28@163.com>
2023-03-11 01:41:18 +0800

    字段排序

--- src/main/java/com/java8/stream/SortedDemo.java
+++ src/main/java/com/java8/stream/SortedDemo.java
@@ -27,6 +27,7 @@ public class SortedDemo {
         Order order2=Order.builder().dateStr("2021-10-01 00:00:00").price(1.3).build();
         Order order3=Order.builder().dateStr("2021-10-01 00:00:00").price(55.3).build();
         Order order4=Order.builder().dateStr("2021-12-01 00:00:00").price(55.3).build();
+        //添加null测试
         Order order5=Order.builder().dateStr(null).price(null).build();
 
         List<Order> list= new ArrayList<>();
@@ -36,20 +37,32 @@ public class SortedDemo {
         list.add(order4);
         list.add(order5);
 
+        // ==========================================================================
+        //naturalOrder()表示自然排序(一般是升序)，没有处理null，排序时可能会空指针
+        Comparator<Order> comparator1 = Comparator.comparing(Order::getDateStr, Comparator.naturalOrder());
+//        List<Order> orderList1 = list.stream().sorted(comparator1).collect(Collectors.toList());
+//        System.out.println("orderList1："+orderList1);
+
+
+        // ==========================================================================
+        //Comparator.reverseOrder表示 降序，没有处理null，排序时可能会空指针
+        Comparator<Order> comparator2 = Comparator.comparing(Order::getDateStr, Comparator.reverseOrder());
+//        List<Order> orderList2 = list.stream().sorted(comparator2).collect(Collectors.toList());
+//        System.out.println("orderList2："+orderList2);
 
         // ==========================================================================
 
-        //naturalOrder()表示自然排序(一般是升序)，Comparator.nullsLast表示如果属性为null，就放到最后面
-        Comparator<Order> comparator =  Comparator.comparing(Order::getDateStr, Comparator.nullsLast(naturalOrder()) );
-        List<Order> orderList = list.stream().sorted(comparator).collect(Collectors.toList());
-//        System.out.println("orderList："+orderList);
+        //Comparator.nullsLast表示如果属性为null，就放到最后面
+        Comparator<Order> comparator3 =  Comparator.comparing(Order::getDateStr, Comparator.nullsLast(naturalOrder()) );
+        List<Order> orderList3 = list.stream().sorted(comparator3).collect(Collectors.toList());
+//        System.out.println("orderList3："+orderList3);
 
         // ==========================================================================
 
-        //Comparator.reverseOrder表示 降序，nullsFirst表示如果属性为null，就放到最前面。
-        Comparator<Order> comparator2 = Comparator.comparing(Order::getDateStr, Comparator.nullsFirst(Comparator.reverseOrder()));
-        List<Order> orderList2 = list.stream().sorted(comparator2).collect(Collectors.toList());
-//        System.out.println("orderList2："+ orderList2);
+        //nullsFirst表示如果属性为null，就放到最前面。
+        Comparator<Order> comparator4 = Comparator.comparing(Order::getDateStr, Comparator.nullsFirst(Comparator.reverseOrder()));
+        List<Order> orderList4 = list.stream().sorted(comparator4).collect(Collectors.toList());
+//        System.out.println("orderList4："+ orderList4);
 
         // ==========================================================================
 
@@ -59,10 +72,10 @@ public class SortedDemo {
         //Order(price=33.3, dateStr=2021-10-01 00:00:00),
         //Order(price=1.3, dateStr=2021-10-01 00:00:00)]
 
-        Comparator<Order> comparator3 = Comparator.comparing(Order::getDateStr, Comparator.nullsLast(Comparator.reverseOrder()))
+        Comparator<Order> comparator5 = Comparator.comparing(Order::getDateStr, Comparator.nullsLast(Comparator.reverseOrder()))
                 .thenComparing(Order::getPrice, Comparator.nullsLast(Comparator.reverseOrder()));
-        List<Order> orderList3 = list.parallelStream().sorted(comparator3).collect(Collectors.toList());
-        System.out.println("orderList3："+ orderList3);
+        List<Order> orderList5 = list.parallelStream().sorted(comparator5).collect(Collectors.toList());
+        System.out.println("orderList5："+ orderList5);
 
 
     }

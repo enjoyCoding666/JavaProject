8d5f4c99bd297ab6c540bce724f7a045c3698889
fenglin <huaiyinnianshao28@163.com>
2023-10-22 11:16:49 +0800

    处理对象

--- src/main/java/com/bean/Address.java
+++ src/main/java/com/bean/Address.java
@@ -1,57 +1,22 @@
 package com.bean;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Optional;
 
 /**
  * @Author: EnjoyCoding
  * @Date: 2020\3\6 0006 23:31
  * @Description:
  */
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 	private String country;
 	private String province;
 	private String city;
 
-	public Address(String country, String province, String city) {
-		this.country = country;
-		this.province = province;
-		this.city = city;
-	}
 
-	public Address() {
-	}
 
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getProvince() {
-		return province;
-	}
-
-	public void setProvince(String province) {
-		this.province = province;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-//	public Optional<String> getCity() {
-//		return Optional.ofNullable(this.city);
-//	}
-
-	public String getCity() {
-		return this.city;
-	}
 }
--- src/main/java/com/bean/User.java
+++ src/main/java/com/bean/User.java
@@ -1,60 +1,20 @@
 package com.bean;
 
-import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by lenovo on  三月
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 	private String name;
 	private String password;
 	private String email;
 	private Address address;
 
-	public Optional<Address> getAddress() {
-		return Optional.ofNullable(address);
-	}
 
-
-	public User() {
-	}
-
-	public User(String name, String password, String email) {
-		this.name = name;
-		this.password = password;
-		this.email = email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-				"name='" + name + '\'' +
-				", password='" + password + '\'' +
-				", email='" + email + '\'' +
-				'}';
-	}
 }
